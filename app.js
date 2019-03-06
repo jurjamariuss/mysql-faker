c967395b280701d6ae3a34d3c2ade7c99b4b5ba2
Marius Jurja <marius.jurja@hype.ro>
2019-03-07 00:14:36 +0200

    Insert

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 let faker = require('faker');
+let mysql = require('mysql');
 
-for(let i=0; i < 10; i++) {
-    console.log(faker.internet.email());
+let connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'username',
+    password: 'password',
+    database: 'test'
+});
+
+let data = [];
+
+for(let i = 0; i < 10; i++) {
+    data.push([
+        faker.name.lastName() + faker.name.firstName(),
+        faker.internet.email(),
+        faker.lorem.sentence()
+    ]);
 }
+
+let q = 'INSERT INTO faker (name, mail, about) VALUES ?';
+
+connection.query(q,[data], function (err, result) {
+    if (err) throw err;
+});
+
+connection.end();
